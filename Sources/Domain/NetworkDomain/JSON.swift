0697ac1c2297366a01cb0765d98936f3c70231e9
Miguel Olmedo <miguel.olmedo@globant.com>
2021-03-04 11:31:32 -0600

    [Feat] Add JSON model

---
+++ Sources/Domain/NetworkDomain/JSON.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miguel Angel Olmedo Perez on 04/03/21.
+//
+import Foundation
+
+public typealias JSON = [String: Any]
+public typealias DecodableJSON<T: Decodable> = [String:T]
